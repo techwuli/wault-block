cf8410348c5042f68ce543317590decfaafebc69
Zhao Yu <zhaoyu@techwuli.com>
2018-03-26 17:48:32 +0800

    init create

---
+++ WaultBlock.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WaultBlock.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
